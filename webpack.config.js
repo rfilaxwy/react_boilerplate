0e02c8e43653d3b95598e9cfaa07cc4a07244f1f
rfilaxwy <ryanfilaxwylie@gmail.com>
2019-01-16 16:09:52 -0700

    Webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -6,15 +8,36 @@ module.exports = {
     },
     output: {
         filename: '[name].[hash].js',
-        path: path.resolve('/dist'),
+        path: path.resolve('./dist'),
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: ['node_modules'],
+                exclude: ['/node_modules'],
                 use: [{loader: 'babel-loader'}],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }],
             }
         ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        open: true,
     }
 }
